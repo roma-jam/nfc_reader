b32a50ea781b05ab5a21effdb2f967b105c61956
roma jam <jam_roma@yahoo.com>
2017-08-20 14:31:58 +0300

    make fill pn frame buffer

--- pn532/pn532.c
+++ pn532/pn532.c
@@ -12,6 +12,23 @@
 #include "../rexos/userspace/stdio.h"
 #include "pn532.h"
 
+#define PN532_FRAME()       ((PN532_START_FRAME*)io_data(pn532->io))
+#define PN532_FRAME_DATA()  ((uint8_t*)(io_data(pn532->io) + sizeof(PN532_START_FRAME)))
+#define PN532_DATA()        ((uint8_t*)(io_data(pn532->io) + sizeof(PN532_START_FRAME) - sizeof(PN532_DATA)))
+#define PN532_POSTAMBULE()  ((PN532_STOP_FRAME*)(io_data(pn532->io) + \
+                                                 sizeof(PN532_START_FRAME) +  \
+                                                 pn532_extract_data_size(pn532) - \
+                                                 sizeof(PN532_DATA)))
+
+
+void pn532_dump(uint8_t* data, uint32_t size, char* S)
+{
+    printf("%s:\n", S);
+    for(uint32_t i = 0; i < size; i++)
+        printf("%02X ", data[i]);
+    printf("\n");
+}
+
 static inline void pn532_power_on(PN532* pn532)
 {
     gpio_set_pin(PN_RST_PIN);
@@ -26,18 +43,84 @@ static inline void pn532_power_off(PN532* pn532)
     pn532->state = PN532_STATE_OFF;
 }
 
+static inline void pn532_put_data_size(PN532* pn532, unsigned int data_size)
+{
+    /*  Length = TxLength + ADataLength + PN_MAX_TFI_PD0_SIZE
+     *  TxLength        - Length from HOST (by CCID)
+     *  ADataLength     - Length from Service CmdBuf (only if ADataLength > 1)
+     */
+    data_size += sizeof(uint8_t) << 1;
+    PN532_FRAME()->data_size_hi = (uint8_t)(data_size >> 8);
+    PN532_FRAME()->data_size_lo = (uint8_t)(data_size);
+    PN532_FRAME()->LCS = -((uint8_t)(data_size >> 8) + (uint8_t)(data_size));
+}
+
+static inline uint16_t pn532_extract_data_size(PN532* pn532)
+{
+    return ((PN532_FRAME()->data_size_hi << 8) | (PN532_FRAME()->data_size_lo));
+}
+
+static inline void pn532_calculate_dcs(PN532* pn532)
+{
+    /* Calc DCS
+     *  [TFI + PD0 + PD1 + … + PDn + DCS] = 0x00
+     */
+    PN532_POSTAMBULE()->DCS = 0;
+
+    for(int i = 0; i < pn532_extract_data_size(pn532); i++)
+    {
+//        printf("calc: %X\n", PN532_DATA()[i]);
+        PN532_POSTAMBULE()->DCS += PN532_DATA()[i];
+    }
+//    printf("DCS: %X\n", PN532_POSTAMBULE()->DCS);
+    PN532_POSTAMBULE()->DCS = -PN532_POSTAMBULE()->DCS;
+//    printf("DCS: %X\n", PN532_POSTAMBULE()->DCS);
+    PN532_POSTAMBULE()->stop_byte = 0x00;
+}
+
+static inline void pn532_prepare_frame(PN532* pn532, uint8_t code, unsigned int data_size)
+{
+    PN532_FRAME()->cmd_type = PN_CMD_TYPE_DATA_WRITE;
+    PN532_FRAME()->start_byte = 0x00;
+    PN532_FRAME()->sopCH = 0x00;
+    PN532_FRAME()->sopCL = 0xFF;
+    PN532_FRAME()->NPLC = 0xFF;
+    PN532_FRAME()->NPL = 0xFF;
+    // prepare data
+    pn532_put_data_size(pn532, data_size);
+    PN532_FRAME()->data.TFI = PN_FRAME_TFI_TRANSMIT;
+    PN532_FRAME()->data.PD0 = code;
+    // fill postamble
+    pn532_calculate_dcs(pn532);
+}
+
+static inline void pn532_cmd(PN532* pn532, uint8_t code, unsigned int data_size, ...)
+{
+    if (data_size)
+    {
+        va_list arg;
+        va_start(arg, data_size);
+        for(int i = 0; i < data_size; i++)
+            PN532_FRAME_DATA()[i] = (uint8_t)va_arg(arg, int);
+        va_end(arg);
+    }
+    // send preambule
+    pn532_prepare_frame(pn532, code, data_size);
+    pn532->io->data_size = sizeof(PN532_START_FRAME) + sizeof(PN532_STOP_FRAME) + data_size;
+    pn532_dump(io_data(pn532->io), pn532->io->data_size, "PN FRAME");
+}
 
 unsigned int pn532_open_hw(PN532* pn532)
 {
 #if (PN_DEBUG)
     printf("PN532 open\n");
 #endif // PN_DEBUG
-    pn532->io = io_create(sizeof(PN532_FRAME));
+    pn532->io = io_create(PN_MAX_CMD_SIZE);
     if(pn532->io == NULL)
     {
         return 1;
     }
-
+    io_reset(pn532->io);
 
     gpio_enable_pin(PN_IRQ_PIN, GPIO_MODE_IN_FLOAT);
     gpio_enable_pin(PN_RST_PIN, GPIO_MODE_OUT);
@@ -52,6 +135,13 @@ unsigned int pn532_open_hw(PN532* pn532)
 
     pn532_power_on(pn532);
 
+    pn532_cmd(pn532, PN_CMD_GET_FIRMWARE_VERSION, 0);                          // First Cmds will be discarted
+    pn532_cmd(pn532, PN_CMD_GET_FIRMWARE_VERSION, 0);
+    pn532_cmd(pn532, PN_CMD_SAM_CONFIGURATION, 1, 0x01);                       // Disable SAM to calm PN: Normal mode, the SAM is not used
+    pn532_cmd(pn532, PN_CMD_RF_CONFIGURATION, 4, 0x05, 0x02, 0x01, 0x05);      // Setup RF config: One retry, ATR: once, PSL: once, Activation: once
+    pn532_cmd(pn532, PN_CMD_RF_CONFIGURATION, 4, 0x02, 0x00, 0x0B, 0x10);      // Setup RtrTimout: 0x0B - ATR_Res timeOut; 0x10 - TimeOutAnswer
+    pn532_cmd(pn532, PN_CMD_RF_CONFIGURATION, 2, 0x04, 0x00);                  // Setup RtrTimeOut: 0x00 - One Retry; 0xFF - Eternally
+
     return 0;
 }
 
@@ -60,8 +150,9 @@ unsigned int pn532_reset(PN532* pn532)
 #if (PN_DEBUG)
     printf("PN532 reset\n");
 #endif // PN_DEBUG
-    pn532_power_off();
-    pn532_power_on();
+    pn532_power_off(pn532);
+    pn532_power_on(pn532);
+    return 0;
 }
 
 unsigned int pn532_close_hw(PN532* pn532)
--- pn532/pn532_config.h
+++ pn532/pn532_config.h
@@ -8,52 +8,135 @@
 #ifndef PN532_CONFIG_H_
 #define PN532_CONFIG_H_
 
-// ================================
-#define PN_DEBUG                1
+// =============================================================================
+#define PN_DEBUG                        1
 
 // ================================ CONFIGURATION ==============================
-#define PN_SPI                  SPI_1
+#define PN_SPI                          SPI_1
 
-#define PN_IRQ_PIN              B1
-#define PN_NSS_PIN              B0
-#define PN_RST_PIN              A4
-#define PN_SCK_PIN              A5
-#define PN_MISO_PIN             A6
-#define PN_MOSI_PIN             A7
+#define PN_IRQ_PIN                      B1
+#define PN_NSS_PIN                      B0
+#define PN_RST_PIN                      A4
+#define PN_SCK_PIN                      A5
+#define PN_MISO_PIN                     A6
+#define PN_MOSI_PIN                     A7
 
-#define PN_MAX_CMD_SIZE         274  // PN max data size: 264 + 8 (Prologue) + 2 (Epilogue)
-#define PN_MAX_DATA_SIZE        262  // Max Data Lenght 262 byte um. page 29
+#define PN_MAX_CMD_SIZE                 274  // PN max data size: 264 + 8 (Prologue) + 2 (Epilogue)
+#define PN_MAX_DATA_SIZE                262  // Max Data Lenght 262 byte um. page 29
+
+// =================================== Constants ==============================
+// Preamble command bytes
+#define PN_CMD_TYPE_DATA_WRITE         (1 << 0)
+#define PN_CMD_TYPE_STATUS_READ        (1 << 1)
+#define PN_CMD_TYPE_DATA_READ          (3 << 0)
+// Frame identifier
+// Pn CMDs
+#define PN_FRAME_PREAMBLE               0x00
+#define PN_FRAME_TFI_TRANSMIT           0xD4
+#define PN_FRAME_TFI_RECIEVE            0xD5
+#define PN_FRAME_POSTAMBLE              0x00
+
+// Pn CMD Length
+#define PN_ACK_NACK_LENGTH              (uint8_t)6
+#define PN_SERVICE_BYTES_IN_DATA_EX     (uint8_t)3
+#define PN_RESPONSE_START_FRAME_LENGTH  (uint8_t)5
+#define PN_RESPONSE_EPILOGUE_LENGTH     (uint8_t)2
+// Command codes
+// Miscellaneous
+#define PN_CMD_DIAGNOSE                 0x00
+#define PN_CMD_GET_FIRMWARE_VERSION     0x02
+#define PN_CMD_GET_GENERAL_STATUS       0x04
+#define PN_CMD_READ_REGISTER            0x06
+#define PN_CMD_WRITE_REGISTER           0x08
+#define PN_CMD_READ_GPIO                0x0C
+#define PN_CMD_WRITE_GPIO               0x0E
+#define PN_CMD_SET_SERIAL_BAUD_RATE     0x10
+#define PN_CMD_SET_PARAMETERS           0x12
+#define PN_CMD_SAM_CONFIGURATION        0x14
+#define PN_CMD_POWER_DOWN               0x16
+// RF communication
+#define PN_CMD_RF_CONFIGURATION         0x32
+#define PN_CMD_RF_REGULATION_TEST       0x58
+// Initiatior
+#define PN_CMD_IN_JUMP_FOR_DEP          0x56
+#define PN_CMD_IN_JUMP_FOR_PSL          0x46
+#define PN_CMD_IN_LIST_PASSIVE_TARGET   0x4A
+#define PN_CMD_IN_ATR                   0x50
+#define PN_CMD_IN_PSL                   0x4E
+#define PN_CMD_IN_DATA_EXCHANGE         0x40
+#define PN_CMD_IN_COMMUNICATE_THRU      0x42
+#define PN_CMD_IN_DESELECT              0x44
+#define PN_CMD_IN_RELEASE               0x52
+#define PN_CMD_IN_SELECT                0x54
+#define PN_CMD_IN_AUTO_POLL             0x60
+// Target
+#define PN_CMD_TG_INIT_AS_TARGET        0x8C
+#define PN_CMD_TG_SET_GENERAL_BYTES     0x92
+#define PN_CMD_TG_GET_DATA              0x86
+#define PN_CMD_TG_SET_DATA              0x8E
+#define PN_CMD_TG_SET_META_DATA         0x94
+#define PN_CMD_TG_GET_INITIATOR_COMMAND 0x88
+#define PN_CMD_TG_RESPONSE_TO_INITIATOR 0x90
+#define PN_CMD_TG_GET_TARGET_STATUS     0x8A
+
+// Replies
+#define PN_RPL_DIAGNOSE                 0x01
+#define PN_RPL_GET_FIRMWARE_VERSION     0x03
+#define PN_RPL_GET_GENERAL_STATUS       0x05
+#define PN_RPL_POWER_DOWN               0x17
+#define PN_RPL_TG_INIT_AS_TARGET        0x8D
+#define PN_RPL_TG_GET_DATA              0x87
+#define PN_RPL_TG_SET_DATA              0x8F
+#define PN_RPL_TG_GET_INITIATOR_COMMAND 0x89
+#define PN_RPL_TG_RESPONSE_TO_INITIATOR 0x91
+#define PN_RPL_TG_GET_TARGET_STATUS     0x8B
+
+#define PN_RPL_READ_REGISTER            0x07
+#define PN_RPL_WRITE_REGISTER           0x09
+#define PN_RPL_IN_LIST_PASSIVE_TARGET   0x4B
+#define PN_RPL_IN_DESELECT              0x45
+#define PN_RPL_IN_SELECT                0x55
+
+// Parameters bits for SetParameters function (p. 83)
+#define PN_PARAM_NAD                        (1 << 0)
+#define PN_PARAM_DID                        (1 << 1)
+#define PN_PARAM_AUTO_ATR_RES               (1 << 2)
+#define PN_PARAM_IRQ                        (1 << 3)
+#define PN_PARAM_AUTO_RATS                  (1 << 4)
+#define PN_PARAM_ISO14443_4_PICC            (1 << 5)
+#define PN_PARAM_REMOVE_PREAMBULEPOSTAMBLE  (1 << 6)
+
+// Mifare commands
+#define MIFARE_CMD_READ                     0x30
+#define MIFARE_CMD_WRITE                    0xA2
 
 #pragma pack(push, 1)
 typedef struct {
-    uint8_t Preamble;       // 0x00
-    uint8_t SoPCH;          // Start of Packet Code Low - 0x00 always
-    uint8_t SoPCL;          // Start of Packet Code High - 0xFF always
-    uint8_t NPLC;           // Normal Packet Length Checksum - 0xFF always
-    uint8_t NPL;            // Normal Packet Length - 0xFF always
-    uint8_t LengthHi;       // MSByte of Length
-    uint8_t LengthLo;       // LSByte of Length
-    uint8_t LCS;            // Length Checksum
-} PN532_PREAMBULA;
-
-typedef struct {
     uint8_t TFI;
     uint8_t PD0;
-    uint8_t Tg;
-    uint8_t Buf[PN_MAX_DATA_SIZE - 1];
+    // Tg - first byte in user data
 } PN532_DATA;
 
 typedef struct {
-    uint8_t DCS;
-    uint8_t Postamble;      // 0x00
-} PN532_POSTAMBULA;
+    uint8_t cmd_type;
+    // preambule
+    uint8_t start_byte;       // 0x00
+    uint8_t sopCH;            // Start of Packet Code High - 0x00 always
+    uint8_t sopCL;            // Start of Packet Code Low - 0xFF always
+    uint8_t NPLC;             // Normal Packet Length Checksum - 0xFF always
+    uint8_t NPL;              // Normal Packet Length - 0xFF always
+    uint8_t data_size_hi;     // MSByte of Length
+    uint8_t data_size_lo;     // LSByte of Length
+    uint8_t LCS;              // Length Checksum
+    // data
+    PN532_DATA data;
+} PN532_START_FRAME;
 
 typedef struct {
-    uint8_t seq;
-    PN532_PREAMBULA preamb;
-    PN532_DATA data;
-    PN532_POSTAMBULA post;
-} PN532_FRAME;
+    // postambule
+    uint8_t DCS;
+    uint8_t stop_byte;      // 0x00
+} PN532_STOP_FRAME;
 #pragma pack(pop)
 
 
